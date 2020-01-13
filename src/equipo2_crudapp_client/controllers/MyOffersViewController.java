eebba33c081d167fcedbefa29d0c82ff9aa904a7
iker lopez carrillo <iker98lopez@gmail.com>
2020-01-13 12:09:38 +0100

    Merged with dev-diego. Started coding MyOffersViewController. Updated SignInView.fxml

--- src/equipo2_crudapp_client/controllers/MyOffersViewController.java
+++ src/equipo2_crudapp_client/controllers/MyOffersViewController.java
@@ -5,10 +5,39 @@
  */
 package equipo2_crudapp_client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
 /**
  *
- * @author Diego Corral
+ * @author Iker Lopez
  */
 public class MyOffersViewController {
     
+    @FXML
+    private TableView tableOffers;
+    
+    @FXML
+    private TableColumn tableColumnSoftware;
+    
+    @FXML
+    private TableColumn tableColumnBasePrice;
+    
+    @FXML
+    private TableColumn tableColumnDiscountedPrice;
+    
+    @FXML
+    private TableColumn tableColumnDiscount;
+    
+    @FXML
+    private TableColumn tableColumnShop;
+    
+    @FXML
+    private TableColumn tableColumnUrl;
+    
+    public void initStage (Stage stage) {
+        
+    }
 }
