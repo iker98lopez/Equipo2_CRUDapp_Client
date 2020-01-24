3ab0eb6c6fb2c0c062324f1c27e9111f806c5998
Trejerum <dicorral28@gmail.con>
2020-01-24 12:41:12 +0100

    tableView shops

--- src/equipo2_crudapp_client/controllers/ShopsViewController.java
+++ src/equipo2_crudapp_client/controllers/ShopsViewController.java
@@ -5,14 +5,26 @@
  */
 package equipo2_crudapp_client.controllers;
 
+import equipo2_crudapp_classes.classes.Shop;
+import equipo2_crudapp_classes.classes.Software;
+import equipo2_crudapp_classes.classes.Wish;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 /**
  * Controller for the shops list view
@@ -42,6 +54,35 @@ public class ShopsViewController{
     private Button buttonClose;
     
     /**
+     * TableView of shops
+     */
+    @FXML
+    private TableView tableViewShops;
+    
+    /**
+     * Table column that shows the shop's name
+     */
+    @FXML
+    private TableColumn tableColumnName;
+    
+    /**
+     * Table column that shows the shop's image
+     */
+    @FXML
+    private TableColumn tableColumnImage;
+    
+    /**
+     * Table column that shows the shop's url
+     */
+    @FXML
+    private TableColumn tableColumnUrl;
+    
+    /**
+     * Set of shops
+     */
+    private Set<Shop> shops = new HashSet<Shop>();
+    
+    /**
      * This method sets the stage
      * @param stage Stage to be set
      */
@@ -64,6 +105,8 @@ public class ShopsViewController{
         stage.show();
         
         buttonClose.setOnAction(this::handleButtonCloseAction);
+        
+        setTableData();
     }
     
     /**
@@ -72,5 +115,27 @@ public class ShopsViewController{
      */
     public void handleButtonCloseAction(ActionEvent event){
         stage.hide();
+    }
+    
+    /**
+     * Method that populates tableView with shops
+     */
+    public void setTableData() {
+
+        // wishes = user.getWishList();          
+        /*shops.add(new Wish(3, new Software("s3"), 3.0));
+        shops.add(new Wish(2, new Software("s2"), 2.0));
+        shops.add(new Wish(1, new Software("s1"), 1.0));*/
+        shops.add(new Shop(1, "Steam", "www.steam.com"));
+        shops.add(new Shop(2, "Epic Games", "www.epicgames.com"));
+
+        tableColumnName.setCellValueFactory(new PropertyValueFactory("name"));
+        tableColumnUrl.setCellValueFactory(new PropertyValueFactory("url"));
+        tableColumnImage.setCellValueFactory(new PropertyValueFactory("image"));
+
+        ObservableList<Shop> observableShops = FXCollections.observableArrayList();
+        observableShops.addAll(shops);
+        tableViewShops.setItems(observableShops);
+
     }
 }
