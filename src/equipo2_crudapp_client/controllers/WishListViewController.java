c0cbc8086e4bc21cbb2e327f86dceccd84c47850
leioa <leioa@DESKTOP-Q812RCD>
2020-01-24 08:36:44 +0100

    How to populate Table info on setTableData() method on WishListViewController

--- src/equipo2_crudapp_client/controllers/WishListViewController.java
+++ src/equipo2_crudapp_client/controllers/WishListViewController.java
@@ -10,8 +10,12 @@ import equipo2_crudapp_classes.classes.Software;
 import equipo2_crudapp_classes.classes.User;
 import equipo2_crudapp_classes.classes.Wish;
 import equipo2_crudapp_client.clients.WishClient;
+import java.util.AbstractSet;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -21,11 +25,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.GenericType;
 
@@ -34,31 +40,31 @@ import javax.ws.rs.core.GenericType;
  * @author Adrián García
  */
 public class WishListViewController extends GenericSideBarController {
-    
+
     /**
      * Client to make petitions
      */
     private static final WishClient CLIENT = new WishClient();
-    
+
     /**
-     * Set of wished used to recieve the wishes 
+     * Set of wishes used to recieve the wishes
      */
-    private Set<Wish> wishes;
+    private Set<Wish> wishes = new HashSet<Wish>();
     /**
      * The user that is logged
      */
     private User user;
-    
+
     /**
      * Logger to output messages to the console
      */
     private static final Logger LOGGER = Logger.getLogger("equipo2_crudapp_client.controllers.WishListViewController");
-   
+
     /**
      * Scene of the controller
      */
     private Scene scene;
-    
+
     /**
      * Table that contains wishes
      */
@@ -77,7 +83,7 @@ public class WishListViewController extends GenericSideBarController {
     /**
      * TextField that recieves a text for search
      */
-    @FXML 
+    @FXML
     private TextField textFieldFilter;
     /**
      * CheckBox that makes the WishList table editable
@@ -89,7 +95,7 @@ public class WishListViewController extends GenericSideBarController {
      */
     @FXML
     private Button buttonFilter;
-    
+
     /**
      * This method initializes the stage and shows the window, sets the
      * visibility of the components and assigns the listeners.
@@ -103,10 +109,11 @@ public class WishListViewController extends GenericSideBarController {
         stage.setScene(scene);
         stage.setTitle("WishList");
         stage.show();
-        
-        buttonFilter.setOnAction(this::handleButtonFilterAction);     
+
+        buttonFilter.setOnAction(this::handleButtonFilterAction);
+        setTableData();
     }
-    
+
     /**
      * This method sets the stage
      *
@@ -115,51 +122,40 @@ public class WishListViewController extends GenericSideBarController {
     public void setStage(Stage stage) {
         this.stage = stage;
     }
-    
+
     /**
      * Method that filters by name in the wishlist
+     *
      * @param event
      */
-    public void handleButtonFilterAction (ActionEvent event) {
-        
+    public void handleButtonFilterAction(ActionEvent event) {
+
     }
-    
+
     public void setTableData() {
-        
-        // wishes = user.getWishList();
-        Wish w = new Wish();
-        Software s = new Software();
-        s.setName("s1");
-        w.setSoftware(s);
-        w.setMinPrice(1.0);
-        
-        Wish w2 = new Wish();
-        Software s2 = new Software();
-        s.setName("s2");
-        w.setSoftware(s2);
-        w.setMinPrice(2.0);
-        
-        Wish w3 = new Wish();
-        Software s3 = new Software();
-        s.setName("s3");
-        w.setSoftware(s3);
-        w.setMinPrice(3.0);
-        
-        wishes.add(w);
-        wishes.add(w2);
-        wishes.add(w3);
-        
-        tableColumnSoftware.setCellValueFactory(new PropertyValueFactory("software.name"));
-        tableColumnMinPrice.setCellFactory(new PropertyValueFactory("minPrice"));
-        
+
+        // wishes = user.getWishList();          
+        wishes.add(new Wish(3, new Software("s3"), 3.0));
+        wishes.add(new Wish(2, new Software("s2"), 2.0));
+        wishes.add(new Wish(1, new Software("s1"), 1.0));
+
+        tableColumnSoftware.setCellValueFactory(new Callback<
+        CellDataFeatures<Wish, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(
+                    CellDataFeatures<Wish, String> data) {
+                return data.getValue().getSoftware().getNameProperty();
+            }
+        });
+        tableColumnMinPrice.setCellValueFactory(new PropertyValueFactory("minPrice"));
+
         ObservableList<Wish> observableWishes = FXCollections.observableArrayList();
         observableWishes.addAll(wishes);
         tableViewWishList.setItems(observableWishes);
-        
-        
+
     }
-    
+
     public void setUser() {
-        
+
     }
 }
